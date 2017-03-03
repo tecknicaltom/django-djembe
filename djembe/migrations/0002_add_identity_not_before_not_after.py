b3b9dd5232ff2bec47e09c78ff7def951c086adc
Tom Samstag <github@modtwo.com>
2017-03-03 14:15:49 -0800

    squash migrations

--- djembe/migrations/0002_add_identity_not_before_not_after.py
+++ djembe/migrations/0002_add_identity_not_before_not_after.py
@@ -3,6 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+from M2Crypto import X509
+
+
+def generate_dates(apps, schema_editor):
+    Identity = apps.get_model("djembe", "Identity")
+    for identity in Identity.objects.all():
+        x509 = X509.load_cert_string(str(identity.certificate))
+        identity.not_before = x509.get_not_before().get_datetime()
+        identity.not_after = x509.get_not_after().get_datetime()
+        identity.save()
 
 class Migration(migrations.Migration):
 
@@ -22,5 +32,16 @@ class Migration(migrations.Migration):
             name='not_before',
             field=models.DateField(help_text='If left blank, it will be extracted from the X.509 certificate.', null=True, blank=True),
             preserve_default=True,
+        ),
+        migrations.RunPython(generate_dates),
+        migrations.AlterField(
+            model_name='identity',
+            name='not_after',
+            field=models.DateField(help_text='If left blank, it will be extracted from the X.509 certificate.', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='identity',
+            name='not_before',
+            field=models.DateField(help_text='If left blank, it will be extracted from the X.509 certificate.', blank=True),
         ),
     ]
